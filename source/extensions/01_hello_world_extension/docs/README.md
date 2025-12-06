50ca0373ea50854de13d42f84141c2b3caf32405
Pyro9803 <nhatminhhoang08092003@gmail.com>
2025-12-06 17:47:03 +0700

    feat: create hello world extension

---
+++ source/extensions/01_hello_world_extension/docs/README.md
@@ -0,0 +1,3 @@
+# My Hello World Python UI Extension [01_hello_world_extension]
+
+A simple python UI extension example. Use it as a starting point for your extensions.
---
+++ source/extensions/01_hello_world_extension/premake5.lua
@@ -0,0 +1,11 @@
+-- Use folder name to build extension name and tag. Version is specified explicitly.
+local ext = get_current_extension_info()
+
+project_ext (ext)
+
+-- Link only those files and folders into the extension target directory
+repo_build.prebuild_link {
+    { "data", ext.target_dir.."/data" },
+    { "docs", ext.target_dir.."/docs" },
+    { "01_hello_world_extension", ext.target_dir.."/01_hello_world_extension" },
+}
